42282237f2afcd77b52bc93e76f5f33e652b2f06
Kenny <Kennygmtz@gmail.com>
2026-05-05 17:33:24 -0300

    feat: aula2 - adiciona metodos com parametros e retorno

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -8,26 +8,24 @@ public class SistemaPrincipal {
 
         Dispositivo notebook = new Dispositivo();
         notebook.tipo = "Notebook";
-        notebook.nome = "Dell Inspiron";
-        notebook.memoria = 16;
+        notebook.nome = "Dell";
+        notebook.memoria = 8;
         notebook.tamanhoTela = 15.6;
+
+        notebook.adicionarMemoria(8);
+
+        int memoriaAtual = notebook.obterMemoria();
+
+        System.out.println("Memória atual: " + memoriaAtual + "GB\n");
+
+        notebook.exibirInfo();
 
         Dispositivo celular = new Dispositivo();
         celular.tipo = "Celular";
-        celular.nome = "Samsung Galaxy";
+        celular.nome = "Samsung";
         celular.memoria = 128;
         celular.tamanhoTela = 6.5;
 
-        System.out.println("=== DISPOSITIVOS CADASTRADOS ===\n");
-
-        System.out.println("Tipo: " + notebook.tipo);
-        System.out.println("Nome: " + notebook.nome);
-        System.out.println("Memória: " + notebook.memoria + " GB");
-        System.out.println("Tela: " + notebook.tamanhoTela + "\"\n");
-
-        System.out.println("Tipo: " + celular.tipo);
-        System.out.println("Nome: " + celular.nome);
-        System.out.println("Memória: " + celular.memoria + " GB");
-        System.out.println("Tela: " + celular.tamanhoTela + "\"");
+        celular.exibirInfo();
     }
 }
--- src/br/com/fiapride/model/Dispositivo.java
+++ src/br/com/fiapride/model/Dispositivo.java
@@ -2,9 +2,30 @@ package br.com.fiapride.model;
 
 public class Dispositivo {
 
-    public String tipo;          // Notebook ou Celular
-    public String nome;          // Marca ou modelo
-    public int memoria;          // RAM (Notebook) ou Armazenamento (Celular)
+    public String tipo;
+    public String nome;
+    public int memoria;
     public double tamanhoTela;
 
+    public void adicionarMemoria(int valor) {
+        memoria = memoria + valor;
+    }
+
+    public int obterMemoria() {
+        return memoria;
+    }
+
+    public void alterarNome(String novoNome) {
+        if (novoNome != null && !novoNome.isEmpty()) {
+            nome = novoNome;
+        }
+    }
+
+    public void exibirInfo() {
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Nome: " + nome);
+        System.out.println("Memória: " + memoria + " GB");
+        System.out.println("Tela: " + tamanhoTela + "\"");
+        System.out.println("-----------------------");
+    }
 }
